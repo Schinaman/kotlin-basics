f58489637736d750e62eaa24f04c0f8d9342f08b
China <guilherme.hiraoka@gmail.com>
2022-12-26 19:05:09 -0300

    feat: Cria projeto, Types transição

---
+++ kotlin-basics/src/main/kotlin/br/com/kotlin/basics/session04/_0403/KotlinTypes.kt
@@ -0,0 +1,24 @@
+package br.com.kotlin.basics.session04._0403
+
+fun main() {
+    val myPhrase = "I'm a String"
+    val myPi = 3.14
+    val myanswer = 42
+
+    val aString: String
+    val aDouble: Double
+    val aInt: Int
+
+    val myDouble = 1.9999
+    val myFloat = 1.9f
+    val myLong = 94164616316546451L
+    val myInt = 546451L
+    val myShort: Short = 5451
+    val myByte: Byte = 54
+
+    val aLongeNumber = 123_456_789
+    val aSameNumber = 123456789
+
+    val anInt: Int = 7
+    val aLong: Long= anInt.toLong() //conversao nao eh implicita
+}
